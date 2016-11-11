ba26f1b367e4cd911c677e1902215d3b9c2f750c
Evghenii <reessult@gmail.com>
2016-11-10 18:17:41 -0800

    first commit

--- src/Database.java
+++ src/Database.java
@@ -1,1 +1,10 @@
-importiuoasdhyaiushdaiusiiiiiimport
+class Database {
+
+  public static void main(String [] argv) {
+    System.out.println("Midway upon the journey of our life");
+    System.out.println("I found myself within a forest dark,");
+    System.out.println("For the straightforward pathway had be lost.");
+    System.out.println("\n Text from Inferno by Dante Alighieri");
+  }
+
+}
