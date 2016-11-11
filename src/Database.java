ad5df5a65d716750c79f1b7caeecc394822161a7
Evghenii <reessult@gmail.com>
2016-11-11 10:51:35 -0800

    started to work on database class

--- src/Database.java
+++ src/Database.java
@@ -5,4 +5,26 @@ import java.util.Vector;
 public class Database {
 	Connection conn = null;
 	
+	//Constructor
+	public Database(String dbName) {
+		dbName = "jdbc:sqlite:" + dbName;
+		
+		//Try to make connection with the database
+		try {
+			Class.forName("org.sqlite.JDBC");
+			conn = DriverManager.getConnection(dbName);
+			
+		} catch (ClassNotFoundException | SQLException e) {
+			System.err.println("Connection with the database failed.");
+			return;
+		}
+		
+		createPatientTable();
+		
+	}
+	
+	private void createPatientTable(){
+		
+	}
+	
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 
 public class Main {
 	public static void main(String[] args){
-		Database s;
+		Database s = new Database("me");
+		
 		
 	}
 }
