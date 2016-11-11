f8bf1fbebe5c3521e8936b75c1bd430e6d9ac98b
Evghenii <reessult@gmail.com>
2016-11-10 18:24:30 -0800

    second commit

--- src/Database.java
+++ src/Database.java
@@ -1,10 +1,6 @@
-class Database {
+import java.sql.*;
 
-  public static void main(String [] argv) {
-    System.out.println("Midway upon the journey of our life");
-    System.out.println("I found myself within a forest dark,");
-    System.out.println("For the straightforward pathway had be lost.");
-    System.out.println("\n Text from Inferno by Dante Alighieri");
-  }
-
+public class Database {
+	
+	
 }
