a35c46fb11dbeb9bef597444dd99ca0f1d7d5c59
Brian Ginsburg <brian@brianginsburg.com>
2016-11-11 22:12:39 -0800

    Add tests and InputException
    Add tests and start of validating constructor.
    Change tabs to spaces.
    Fix simpler constructor params and tests.

--- src/Service.java
+++ src/Service.java
@@ -1,54 +1,64 @@
 class Service {
-	String name;
-	int code;
-	Double fee;
-	boolean status;
-	
-	// add Validated-constructor for objects from DB Wrapper
+  String name;
+  int code;
+  Double fee;
+  Boolean status;
 
-	public Service(String name, int code, double fee) {
-		setName(name);
-		setCode(code);
-		setFee(fee);
-		setStatus(true);
-		return;
-	}
+  // makes new objects
+  public Service(String name, Double fee) {
+    setName(name);
+    setCode(0);
+    setFee(fee);
+    setStatus(true);
+  }
 
-	private void setName(String name) {
-		this.name = name;
-	}
-	
-	private void setCode(int code) {
-		this.code = code;
-	}
-	
-	private void setFee(Double fee) {
-		this.fee = fee;
-	}
-	
-	private void setStatus(boolean status) {
-		this.status = status;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	
-	public int getCode() {
-		return code;
-	}
-	
-	public double getFee() {
-		return fee;
-	}
-	
-	public boolean getStatus() {
-		return status;
-	}
-	
-	@Override
-	public String toString() {
-		return code + "\t" + name + "\t$" + fee;
-	}
-	
+  // makes objects for DB Wrapper
+  public Service(int code, String name, Double fee, int status) {
+    setName(name);
+    setCode(code);
+    setFee(fee);
+    if (status == 1) {
+      setStatus(true);
+    } else {
+      setStatus(false);
+    }
+  }
+
+  private void setName(String name) {
+    this.name = name;
+  }
+
+  private void setCode(int code) {
+    this.code = code;
+  }
+
+  private void setFee(Double fee) {
+    this.fee = fee;
+  }
+
+  private void setStatus(Boolean status) {
+    this.status = status;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public int getCode() {
+    return code;
+  }
+
+  public double getFee() {
+    return fee;
+  }
+
+  public Boolean getStatus() {
+    return status;
+  }
+
+  @Override
+  public String toString() {
+    return code + "\t" + name + "\t$" + fee;
+  }
+
 }
--- tests/TestService.java
+++ tests/TestService.java
@@ -3,50 +3,84 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class TestService {
-	
-	@Test
-	public void testService() {
-		try {
-			new Service("Massage", 1, 60.85);
-		} catch (Exception e) {
-			fail(e.getMessage());
-		}
-	}
-	
-	@Test
-	public void testName() {
-		Service service = new Service("Massage", 0, 0);
-		String result = service.getName();
-		assertEquals("Massage", result);
-	}
-	
-	@Test
-	public void testCode() {
-		Service service = new Service("", 1, 0);
-		int result = service.getCode();
-		assertEquals(1, result);
-	}
-	
-	@Test
-	public void testFee() {
-		Service service = new Service("", 0, 60.85);
-		Double expectedResult = 60.85;
-		Double actualResult = service.getFee();
-		assertEquals(expectedResult, actualResult);
-	}
-	
-	@Test
-	public void testStatus() {
-		Service service = new Service("", 0, 60.85);
-		boolean result = service.getStatus();
-		assertEquals(true, result);
-	}
 
-	@Test 
-	public void testToString() {
-		Service service = new Service("Massage", 1, 60.85);
-		String expectedResult = "1\tMassage\t$60.85";
-		String actualResult = service.toString();
-		assertEquals(expectedResult, actualResult);
-	}
+  // Test constructor that makes new objects
+  @Test
+  public void testServiceNew() {
+    try {
+      new Service("Massage", 60.85);
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+
+  // Test constructor that makes objects for DB Wrapper,
+  // validates all variables
+  @Test
+  public void testService() {
+    try {
+      new Service(1, "Massage", 60.85, 0);
+    } catch (Exception e) {
+      fail(e.getMessage());
+    }
+  }
+
+  @Test
+  public void testServiceActive() {
+    Service service = new Service(1, "Massage", 60.85, 1);
+    boolean result = service.getStatus();
+    assertEquals(true, result);
+  }
+
+  @Test
+  public void testServiceInactive() {
+    Service service = new Service(1, "Massage", 60.85, 0);
+    boolean result = service.getStatus();
+    assertEquals(false, result);
+  }
+
+  @Test
+  public void testServiceCodeNegative() {
+    Service service = new Service(-1, "Massage", 60.85, 0);
+
+  }
+
+
+
+  @Test
+  public void testName() {
+    Service service = new Service("Massage", 0.0);
+    String result = service.getName();
+    assertEquals("Massage", result);
+  }
+
+  @Test
+  public void testCode() {
+    Service service = new Service("", 0.0);
+    int result = service.getCode();
+    assertEquals(0, result);
+  }
+
+  @Test
+  public void testFee() {
+    Service service = new Service("", 60.85);
+    Double expectedResult = 60.85;
+    Double actualResult = service.getFee();
+    assertEquals(expectedResult, actualResult);
+  }
+
+  @Test
+  public void testStatus() {
+    Service service = new Service("", 60.85);
+    Boolean result = service.getStatus();
+    assertEquals(true, result);
+  }
+
+  @Test
+  public void testToString() {
+    Service service = new Service("Massage", 60.85);
+    String expectedResult = "0\tMassage\t$60.85";
+    String actualResult = service.toString();
+    assertEquals(expectedResult, actualResult);
+  }
 }
