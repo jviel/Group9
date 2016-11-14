fa6311221bb58fa25c4181f15fce9db2b6274635
Johnny Viel <jviel@pdx.edu>
2016-11-13 18:47:20 -0800

    fully implement getInt() getString() and getFloat() with error checking

--- src/Terminal.java
+++ src/Terminal.java
@@ -31,22 +31,21 @@ public class Terminal {
         System.out.println("Hey andy");
         managerTerminal();
 
-        int x = 0;
-        while (true) {
-            x = getInt("please enter an int: ", 0, 25);
-            System.out.println(x);
-        }
+        int x    = getInt("please enter an int: ", 0, 9999);
+        System.out.println(x);
+        String s = getString("please enter a name: ", 0, 25);
+        System.out.println(s);
+        Float f  = getFloat("please enter a decimal number: ", 0f, 99999.99f);
+        System.out.println(f);
     }
 
 
     /**
      * Description
-     *
      */
-
     private static void managerTerminal(){
 
-        System.out.println("In maanger terminal");
+        System.out.println("In manger terminal");
 
     }
 
@@ -67,13 +66,35 @@ public class Terminal {
 
     }
 
+    /**
+     * For getting user input in the form of a string fitting a certain length. Error checking within
+     * @param String prompt to display to user
+     * @param int minimum allowable length
+     * @param int maximum allowable length
+     */
     private static String getString(String prompt, int min, int max)
     {
+        String ret = "";
+        boolean valid = false;
 
-        return "";
+        while(!valid)
+        {
+            System.out.print(prompt);
+            ret = sc.nextLine(); if (ret.length() < min || ret.length() > max)
+                System.out.println("Input length must be between " + min + " and " + max + " characters");
+            else
+                valid = true;
+        }
+        return ret;
     }
 
-    private static int getInt(String prompt, int min, int max) throws InputMismatchException
+    /**
+     * For getting user input in the form of an int within given range. Error checking within
+     * @param String prompt to display to user
+     * @param int minimum allowable value
+     * @param int maximum allowable value
+     */
+    private static int getInt(String prompt, int min, int max)
     {
         int ret = -1;
         boolean valid = false;
@@ -83,23 +104,57 @@ public class Terminal {
             try {
                 System.out.print(prompt);
                 ret = sc.nextInt(); // throws InputMismatchException if not an int
-                if (ret < min || ret > max) {
+                if (ret < min || ret > max)
                     throw new InputMismatchException();
-                } else {
+                else
                     valid = true;
-                }
             }
             catch (InputMismatchException ex) {
                 System.out.println("Value must be a number between " + min + " and " + max);
-                sc.nextLine();
             }
+            clearScanner(sc);
         }
         return ret;
     }
 
-    private static Float getFloat()
+    /**
+     * For getting user input in the form of a float within given range. Error checking within
+     * @param String prompt to display to user
+     * @param Float minimum allowable value
+     * @param Float maximum allowable value
+     */
+    private static Float getFloat(String prompt, Float min, Float max)
     {
+        Float ret = -1f;
+        boolean valid = false;
 
-        return 0f;
+        while(!valid)
+        {
+            try {
+                System.out.print(prompt);
+                ret = sc.nextFloat(); // throws InputMismatchException if not a float
+                if (ret < min || ret > max)
+                    throw new InputMismatchException();
+                else
+                    valid = true;
+            }
+            catch (InputMismatchException ex) {
+                System.out.println("Value must be a decimal number between " + min + " and " + max);
+            }
+            clearScanner(sc);
+        }
+        return ret;
+    }
+
+    /**
+     * When getting numerical input from user, if they enter a non-number, an exception is thrown but buffer
+     * retains input including '\n' character. This needs to be cleared to enter a new number.
+     * Does not apply after Scanner.nextLine() because this pops the '\n'
+     * @param Scanner
+     */
+    private static void clearScanner(Scanner s)
+    {
+        if (s.hasNextLine()) // presence of a newline character.
+            s.nextLine();
     }
 }
