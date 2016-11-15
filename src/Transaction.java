1744af6e080c406662c64ffe8ade936109634767
lc893 <lchoi@pdx.edu>
2016-11-15 01:18:44 -0800

    Add constructors, getters, and setters.
    Add test for first constructor.

--- src/Transaction.java
+++ src/Transaction.java
@@ -1,20 +1,146 @@
+package src;
 
+public class Transaction {
 
-class Transaction {
-
-    // vars
-
+    // variables
+	int idNumber;
+	int patientID;
+	int providerID;
+	int serviceID;
+	int consultationNumber;
+	String dateTime;
+	String serviceDate;
+	String comments;
 
     // constructors
+	public Transaction(int idNumber, int patientID, int providerID, int serviceID, 
+			int consultationNumber, String dateTime, String serviceDate, 
+			String comments) throws InputException {
+	        String exceptionString = "";
+
+	        if (idNumber < 0 || idNumber > 999999999) {
+	            exceptionString += "ID Number must be a nine-digit integer.\n";
+	        }
+	        
+	        if (patientID < 0 || patientID > 999999999) {
+	            exceptionString += "Patient ID must be a nine-digit integer.\n";
+	        }
+	        
+	        if (consultationNumber < 0 || consultationNumber > 999999999) {
+	            exceptionString += "Consultation ID must be a nine-digit integer.\n";
+	        }
+	        
+	        if (providerID < 0 || providerID > 999999999) {
+	            exceptionString += "Provider ID must be a nine-digit integer.\n";
+	        }
+	        
+	        if (serviceID < 0 || serviceID > 999999) {
+	            exceptionString += "Service ID must be a six-digit integer.\n";
+	        }
+
+	        if (dateTime.length() != 19) {
+	            exceptionString += "Date and Time must be formatted MM-DD-YYYY HH:MM:SS.\n";
+	        }
+
+	        if (serviceDate.length() != 10) {
+	            exceptionString += "Service date must be formatted MM-DD-YYYY.\n";
+	        }
+	        
+	        if (comments.length() > 100) {
+	            exceptionString += "Comments may be no longer than 100 characters.\n";
+	        }
+
+	        if (!(exceptionString.isEmpty())) {
+	            exceptionString = exceptionString.substring(0,
+	                exceptionString.length() -1);
+	            throw new InputException(exceptionString);
+	        }
+
+	        setIDNumber(idNumber);
+	        setPatientID(patientID);
+	        setProviderID(providerID);
+	        setServiceID(serviceID);
+	        setConsultationNumber(consultationNumber);
+	        setDateTime(dateTime);
+	        setServiceDate(serviceDate);
+	        setComments(comments); 
+	    
+	}
+	
+	public Transaction(int patientID, int providerID, int serviceID, 
+			int consultationNumber, String serviceDate, 
+			String comments) throws InputException {
+        this(0, patientID, providerID, serviceID, consultationNumber, "00-00-0000 00:00:00", serviceDate, comments);
+    }
 
 
+	// getters
+	public int getIDNumber() {
+		return idNumber;
+	}
+	
+	public int getPatientID() {
+		return patientID;
+	}
+	
+	public int getConsultationNumber() {
+		return consultationNumber;
+	}
+	
+	public int getProviderID() {
+		return providerID;
+	}
+	
+	public int getServiceID() {
+		return serviceID;
+	}
+	
+	public String getDateTime() {
+		return dateTime;
+	}
+	
+	public String getServiceDate() {
+		return serviceDate;
+	}
+	
+	public String getComments() {
+		return comments;
+	}
 
-    // getters
-
-
-
+	
     // setters
-
+	private void setIDNumber(int idNumber) {
+		this.idNumber = idNumber;
+	}
+	
+	private void setPatientID(int patientID) {
+		this.patientID = patientID;
+	}
+	
+	private void setProviderID(int providerID) {
+		this.providerID = providerID;
+	}
+	
+	private void setServiceID(int serviceID) {
+		this.serviceID = serviceID;
+	}
+	
+	private void setConsultationNumber(int consultationNumber) {
+		this.consultationNumber = consultationNumber;
+	}
+	
+	private void setDateTime(String dateTime) {
+		this.dateTime = dateTime;
+	}
+	
+	private void setServiceDate(String serviceDate) {
+		this.serviceDate = serviceDate;
+	}
+	
+	private void setComments(String comments) {
+		this.comments = comments;
+	}
+	
 
 
     // helper functions
@@ -22,6 +148,18 @@ class Transaction {
 
 
     // override toString
+	@Override
+    public String toString() {
+        return "Transaction ID: " + idNumber + "\n" +
+            "Date-Time: " + dateTime + "\n" +
+            "Service Date: " + serviceDate + "\n" +
+            "Provider ID: " + Integer.toString(providerID) + "\n" +
+            "Patient ID: " + Integer.toString(patientID) + "\n" +
+            "Service ID: " + Integer.toString(serviceID) + "\n" +
+            "Consult ID: " + Integer.toString(consultationNumber) + "\n" +
+            (!(comments.isEmpty()) ? "Comment: " : "") +
+            comments;
+    }
 
 
 
--- tests/TestTransaction.java
+++ tests/TestTransaction.java
@@ -1,15 +1,35 @@
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import src.InputException;
+import src.Service;
+import src.Transaction;
 
 public class TestTransaction {
 
     // ---- Test constructors ----
     // test constructor that makes new objects
-
+	@Test
+    public void testTransactionNew() {
+        try {
+            new Transaction(123456789, 234567891, 345678912, 456789, 567891234, "09-08-1876 11:11:11", "12-10-2345", 
+            		"The service was great!");
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
 
     // test constructor that makes objects for DB Wrapper,
     // validates all variables
+	@Test
+    public void testTransaction() {
+        try {
+            new Transaction(234567891, 345678912, 456789, 567891234, "12-10-2345", 
+            		"The service was great!");
+        } catch (Exception e) {
+            fail(e.getMessage());
+        }
+    }
 
 
 
